33541ddd406d9cbc2d3a55c03586f638f13f5627
Bruno Oliveira <brunocicom@gmail.com>
2022-09-30 15:28:43 -0300

    gRPC client

---
+++ cmd/client/client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/brunocicom/fc_grpc/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC Server: %v", err)
+	}
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Bruno",
+		Email: "bruno@email.com",
+	}
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+	fmt.Println(res)
+}
