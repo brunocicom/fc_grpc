b2eab8d9fd496e31116f440e69202c18fbbb0c04
Bruno Oliveira <brunocicom@gmail.com>
2022-09-30 18:02:44 -0300

    Client Stream to Server

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"time"
 
 	"github.com/brunocicom/fc_grpc/pb"
 	"google.golang.org/grpc"
@@ -19,7 +20,9 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	// AddUser(client)
-	AddUserVerbose(client)
+	// AddUserVerbose(client)
+	AddUsers(client)
+
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -57,5 +60,56 @@ func AddUserVerbose(client pb.UserServiceClient) {
 			log.Fatalf("Could not receive the msg: %v", err)
 		}
 		fmt.Println("Status:", stream.Status, " - ", stream.GetUser())
+	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := getMockUsers()
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+
+	fmt.Println(res)
+}
+
+func getMockUsers() []*pb.User {
+	return []*pb.User{
+		&pb.User{
+			Id:    "b1",
+			Name:  "bruno",
+			Email: "bruno@email.com",
+		},
+		&pb.User{
+			Id:    "b2",
+			Name:  "bruno 2",
+			Email: "bruno2@email.com",
+		},
+		&pb.User{
+			Id:    "b3",
+			Name:  "bruno 3",
+			Email: "bruno3@email.com",
+		},
+		&pb.User{
+			Id:    "b4",
+			Name:  "bruno 4",
+			Email: "bruno4@email.com",
+		},
+		&pb.User{
+			Id:    "v5",
+			Name:  "bruno 5",
+			Email: "bruno5@email.com",
+		},
 	}
 }
